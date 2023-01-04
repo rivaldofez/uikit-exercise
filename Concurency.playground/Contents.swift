fbf2a38efcbf72486db3910fcaf08be0a9adf449
rivaldofez <rivaldofez@gmail.com>
2023-01-04 17:02:12 +0700

    change non thread safe to safe

--- Concurency.playground/Contents.swift
+++ Concurency.playground/Contents.swift
@@ -30,3 +30,22 @@ DispatchQueue.main.async {
     }
 }
 
+class ThreadSafeArray {
+    let isolation = DispatchQueue(label: "com.dicoding.dispatchbarrier", attributes: .concurrent)
+    
+    private var _array: [Int] = []
+    
+    var array: [Int] {
+        get {
+            return isolation.sync {
+                _array
+            }
+        }
+        set {
+            isolation.async(flags: .barrier) {
+                self._array = newValue
+            }
+        }
+    }
+}
+
