688d30a02552e1d1a3670c654b53520e66723b96
rivaldofez <rivaldofez@gmail.com>
2023-01-04 17:13:28 +0700

    exercise dispatch group

--- Concurency.playground/Contents.swift
+++ Concurency.playground/Contents.swift
@@ -49,3 +49,56 @@ class ThreadSafeArray {
     }
 }
 
+var value = 5
+let workItem = DispatchWorkItem {
+    value += 5
+}
+
+workItem.perform()
+
+
+let queue = DispatchQueue(label: "com.dicoding.dispatchworkitem", qos: .utility)
+queue.async(execute: workItem)
+
+workItem.notify(queue: DispatchQueue.main) {
+    print("Final value: \(value)")
+}
+
+
+func task1(dispatchGroup: DispatchGroup) {
+   let queue = DispatchQueue(label: "com.dicoding.dispatchgroup.task1")
+ 
+   queue.async {
+        sleep(1)
+        print("Task 1 executed")
+        dispatchGroup.leave()
+    }
+}
+ 
+func task2(dispatchGroup: DispatchGroup) {
+    DispatchQueue.global().async {
+        sleep(2)
+        print("Task 2 executed")
+        dispatchGroup.leave()
+    }
+}
+ 
+func task3(dispatchGroup: DispatchGroup) {
+    DispatchQueue.main.async {
+        print("Task 3 executed")
+        dispatchGroup.leave()
+    }
+}
+
+let dispatchGroup = DispatchGroup()
+
+dispatchGroup.enter()
+task1(dispatchGroup: dispatchGroup)
+dispatchGroup.enter()
+task2(dispatchGroup: dispatchGroup)
+dispatchGroup.enter()
+task3(dispatchGroup: dispatchGroup)
+
+dispatchGroup.notify(queue: DispatchQueue.main) {
+   print("All task finished")
+}
