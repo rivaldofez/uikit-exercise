ed4e2bab37e6a7ce1d18942d82f12b0859252abb
rivaldofez <rivaldofez@gmail.com>
2023-01-04 13:16:53 +0700

    removing storyboard and using root view controller

---
+++ DeclarativeUI/DeclarativeUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeclarativeUI
+//
+//  Created by Rivaldo Fernandes on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
