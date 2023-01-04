b697514cac5d5548899cc8fe581201de7bd23ef4
rivaldofez <rivaldofez@gmail.com>
2023-01-04 20:50:41 +0700

    create project image loader with gcd

---
+++ ImageLoaderGcd/ImageLoaderGcd/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ImageLoaderGcd
+//
+//  Created by Rivaldo Fernandes on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var movieTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
