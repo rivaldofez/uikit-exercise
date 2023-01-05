c3805c757d04ea2ab56f2159aef2ce2e24b038ff
rivaldofez <rivaldofez@gmail.com>
2023-01-05 19:02:15 +0700

    parsing json array

--- APICodable.playground/Contents.swift
+++ APICodable.playground/Contents.swift
@@ -9,13 +9,37 @@ struct MovieResponses: Codable {
     let page: Int
     let totalResults: Int
     let totalPages: Int
+    let movies: [MovieResponse]
     
     enum CodingKeys: String, CodingKey {
         case page
         case totalResults = "total_results"
         case totalPages = "total_pages"
+        case movies = "results"
     }
 }
+
+struct MovieResponse: Codable {
+    let popularity: Double
+    let posterPath: String
+    let title: String
+    let genres: [Int]
+    let voteAverage: Double
+    let overview: String
+    let releaseDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case popularity
+        case posterPath = "poster_path"
+        case title
+        case genres = "genre_ids"
+        case voteAverage = "vote_average"
+        case overview
+        case releaseDate = "release_date"
+    }
+}
+
+
 
 func decodeJSON(from data: Data) {
     let decoder = JSONDecoder()
@@ -27,6 +51,13 @@ func decodeJSON(from data: Data) {
         print("PAGE: \(movies.page)")
         print("TOTAL RESULTS: \(movies.totalResults)")
         print("TOTAL PAGES: \(movies.totalPages)")
+        
+        movies.movies.forEach { movie in
+            print("TITLE: \(movie.title)")
+            print("POSTER: \(movie.posterPath)")
+            print("DATE: \(movie.releaseDate)")
+        }
+        
     } else {
         print("ERROR: Can't Decode JSON")
     }
