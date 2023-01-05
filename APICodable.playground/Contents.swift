83a240f7d8100a2a1a85247b8c6a792d51e96108
rivaldofez <rivaldofez@gmail.com>
2023-01-05 19:00:02 +0700

    using coding key to remapping key json object

--- APICodable.playground/Contents.swift
+++ APICodable.playground/Contents.swift
@@ -6,47 +6,53 @@ let language = "en-US"
 let page = "1"
 
 struct MovieResponses: Codable {
-  let page: Int
-  let total_results: Int
-  let total_pages: Int
+    let page: Int
+    let totalResults: Int
+    let totalPages: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+    }
 }
 
 func decodeJSON(from data: Data) {
-  let decoder = JSONDecoder()
- 
-  if let movies = try? decoder.decode(
-    MovieResponses.self,
-    from: data
-  ) as MovieResponses {
-    print("PAGE: \(movies.page)")
-    print("TOTAL RESULTS: \(movies.total_results)")
-    print("TOTAL PAGES: \(movies.total_pages)")
-  } else {
-    print("ERROR: Can't Decode JSON")
-  }
+    let decoder = JSONDecoder()
+    
+    if let movies = try? decoder.decode(
+        MovieResponses.self,
+        from: data
+    ) as MovieResponses {
+        print("PAGE: \(movies.page)")
+        print("TOTAL RESULTS: \(movies.totalResults)")
+        print("TOTAL PAGES: \(movies.totalPages)")
+    } else {
+        print("ERROR: Can't Decode JSON")
+    }
 }
 
 var components = URLComponents(string: "https://api.themoviedb.org/3/movie/popular")!
 
 components.queryItems = [
-  URLQueryItem(name: "api_key", value: apiKey),
-  URLQueryItem(name: "language", value: language),
-  URLQueryItem(name: "page", value: page)
+    URLQueryItem(name: "api_key", value: apiKey),
+    URLQueryItem(name: "language", value: language),
+    URLQueryItem(name: "page", value: page)
 ]
 
 let request = URLRequest(url: components.url!)
 
 let task = URLSession.shared.dataTask(with: request) { data, response, error in
-  guard let response = response as? HTTPURLResponse else { return }
- 
-  if let data = data {
-    if response.statusCode == 200 {
-      print("DATA: \(data)")
-        decodeJSON(from: data)
-    } else {
-      print("ERROR: \(data), HTTPS Status: \(response.statusCode)")
+    guard let response = response as? HTTPURLResponse else { return }
+    
+    if let data = data {
+        if response.statusCode == 200 {
+            print("DATA: \(data)")
+            decodeJSON(from: data)
+        } else {
+            print("ERROR: \(data), HTTPS Status: \(response.statusCode)")
+        }
     }
-  }
 }
 
 task.resume()
