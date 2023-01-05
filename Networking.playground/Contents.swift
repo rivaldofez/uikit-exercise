ca1115c2322e3594371cdfb87dd432d65b58eaeb
rivaldofez <rivaldofez@gmail.com>
2023-01-05 15:58:50 +0700

    create playground networking

---
+++ Networking.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import SystemConfiguration
+ 
+func isNetworkReachable(with flags: SCNetworkReachabilityFlags) -> Bool {
+    let isReachable = flags.contains(.reachable)
+    let needsConnection = flags.contains(.connectionRequired)
+    let canConnectAutomatically = flags.contains(.connectionOnDemand) || flags.contains(.connectionOnTraffic)
+    let canConnectWithoutUserInteraction = canConnectAutomatically && !flags.contains(.interventionRequired)
+    return isReachable && (!needsConnection || canConnectWithoutUserInteraction)
+}
