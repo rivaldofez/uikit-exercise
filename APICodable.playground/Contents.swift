d19eed6a619f8a1a995be83b90cd468ff75db377
rivaldofez <rivaldofez@gmail.com>
2023-01-05 18:57:54 +0700

    implement json decoder

--- APICodable.playground/Contents.swift
+++ APICodable.playground/Contents.swift
@@ -1,9 +1,30 @@
 import UIKit
 
 // MARK: Gunakan API Key dalam akun Anda.
-let apiKey = "KEY"
+let apiKey = ""
 let language = "en-US"
 let page = "1"
+
+struct MovieResponses: Codable {
+  let page: Int
+  let total_results: Int
+  let total_pages: Int
+}
+
+func decodeJSON(from data: Data) {
+  let decoder = JSONDecoder()
+ 
+  if let movies = try? decoder.decode(
+    MovieResponses.self,
+    from: data
+  ) as MovieResponses {
+    print("PAGE: \(movies.page)")
+    print("TOTAL RESULTS: \(movies.total_results)")
+    print("TOTAL PAGES: \(movies.total_pages)")
+  } else {
+    print("ERROR: Can't Decode JSON")
+  }
+}
 
 var components = URLComponents(string: "https://api.themoviedb.org/3/movie/popular")!
 
@@ -21,6 +42,7 @@ let task = URLSession.shared.dataTask(with: request) { data, response, error in
   if let data = data {
     if response.statusCode == 200 {
       print("DATA: \(data)")
+        decodeJSON(from: data)
     } else {
       print("ERROR: \(data), HTTPS Status: \(response.statusCode)")
     }
@@ -28,3 +50,5 @@ let task = URLSession.shared.dataTask(with: request) { data, response, error in
 }
 
 task.resume()
+
+
