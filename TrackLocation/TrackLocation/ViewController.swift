28fa4ff91a1557610d7ce0d0600a69059e5ba05c
rivaldofez <rivaldofez@gmail.com>
2023-01-06 17:59:35 +0700

    activate background modes for location tracking

--- TrackLocation/TrackLocation/ViewController.swift
+++ TrackLocation/TrackLocation/ViewController.swift
@@ -1,65 +1,66 @@
-//
-//  ViewController.swift
-//  TrackLocation
-//
-//  Created by Rivaldo Fernandes on 06/01/23.
-//
-
 import UIKit
 import MapKit
-
+ 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var mapView: MKMapView!
-    
-    private var locations: [MKPointAnnotation] = []
-    private lazy var locationManager: CLLocationManager = {
-        let manager = CLLocationManager()
-        manager.delegate = self
-        manager.requestAlwaysAuthorization()
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.allowsBackgroundLocationUpdates = true
-        return manager
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
+ 
+  // MARK: Inisialisasi MapView.
+  @IBOutlet weak var mapView: MKMapView!
+ 
+  // MARK: Inisialisasi Array MKPointAnnotion untuk menampung pin yang nantinya dimunculkan di MapView.
+  private var locations: [MKPointAnnotation] = []
+ 
+  // MARK: Mengatur Location Manager.
+  private lazy var locationManager: CLLocationManager = {
+    let manager = CLLocationManager()
+    manager.delegate = self
+    manager.requestAlwaysAuthorization()
+    manager.desiredAccuracy = kCLLocationAccuracyBest
+    manager.allowsBackgroundLocationUpdates = true
+    return manager
+  }()
+ 
+  // MARK: Menghidupkan dan mematikan Track Location.
+  @IBAction func trackSwitch(_ sender: UISwitch) {
+    if sender.isOn {
+      locationManager.startUpdatingLocation()
+    } else {
+      locationManager.stopUpdatingLocation()
     }
-    
-    @IBAction func trackSwitch(_ sender: UISwitch) {
-        if sender.isOn {
-            locationManager.startUpdatingLocation()
-        } else {
-            locationManager.stopUpdatingLocation()
-        }
-    }
-    
+  }
 }
-
+ 
+// MARK: Ini adalah implementasi dari CLLocationManagerDelegate.
 extension ViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let mostRecentLocation = locations.last else { return }
-        
-        let annotationToAdd = MKPointAnnotation()
-        annotationToAdd.coordinate = mostRecentLocation.coordinate
-        
-        self.locations.append(annotationToAdd)
-        
-        while self.locations.count > 50 {
-            if let annotationToRemove = self.locations.first {
-                self.locations.remove(at: 0)
-                mapView.removeAnnotation(annotationToRemove)
-            }
-        }
-        
-        mapView.showAnnotations(self.locations, animated: true)
-        
-        if UIApplication.shared.applicationState == .background {
-            print("Aplikasi dalam background state. Lokasi baru saat ini ", mostRecentLocation)
-        }
+ 
+  func locationManager(
+    _ manager: CLLocationManager,
+    didUpdateLocations locations: [CLLocation]
+  ) {
+ 
+    // MARK: Mendapatkan lokasi terakhir pengguna.
+    guard let mostRecentLocation = locations.last else { return }
+ 
+    // MARK: Membuat variabel untuk menampung latitude dan longitude.
+    let annotationToAdd = MKPointAnnotation()
+    annotationToAdd.coordinate = mostRecentLocation.coordinate
+ 
+    // MARK: Menambahkan lokasi terakhir ke array Locations.
+    self.locations.append(annotationToAdd)
+ 
+    // MARK: Ketika titik lebih dari 50 akan dihapus dari MapView
+    while self.locations.count > 50 {
+      if let annotationToRemove = self.locations.first {
+        self.locations.remove(at: 0)
+        mapView.removeAnnotation(annotationToRemove)
+      }
     }
+ 
+    // MARK: Memunculkan lokasi terakhir di MapView.
+    mapView.showAnnotations(self.locations, animated: true)
+ 
+    // MARK: Ketika Track Location berjalan di background akan mencetak informasi detailnya.
+    if UIApplication.shared.applicationState == .background {
+      print("Aplikasi dalam background state. Lokasi baru saat ini ", mostRecentLocation)
+    }
+  }
 }
-
