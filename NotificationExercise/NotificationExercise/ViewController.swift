9bf45301b3c341f9490439422a8d4f05f31fa10f
rivaldofez <rivaldofez@gmail.com>
2023-01-12 20:13:56 +0700

    add authorization request for notifications

--- NotificationExercise/NotificationExercise/ViewController.swift
+++ NotificationExercise/NotificationExercise/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UNUserNotificationCenter.current().requestAuthorization(options : [.alert, .badge, .sound]){ (granted, _) in
+            if granted {
+                print("Mendapatkan izin dari pengguna untuk local notifications")
+            }else{
+                print("Tidak mendapatkan izin dari pengguna untuk local notifications")
+            }
+        }
+    }
+    
+    
 }
 
