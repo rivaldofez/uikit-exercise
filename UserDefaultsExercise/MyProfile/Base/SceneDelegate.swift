f39b20b0f8dfb751389d04cb602ff5a412254029
rivaldofez <rivaldofez@gmail.com>
2023-01-12 16:37:08 +0700

    complete implement form edit, save and delete with user defaults

--- UserDefaultsExercise/MyProfile/Base/SceneDelegate.swift
+++ UserDefaultsExercise/MyProfile/Base/SceneDelegate.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
-
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-  var window: UIWindow?
-
+    
+    var window: UIWindow?
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+       
+        let initialViewController: UIViewController?
+        
+        window = UIWindow(windowScene: windowScene)
+        if ProfileModel.stateLogin {
+            initialViewController = storyboard
+                .instantiateViewController(withIdentifier: "HomeScene") as? HomeViewController
+        } else {
+            initialViewController = storyboard
+                .instantiateViewController(withIdentifier: "CreateScene") as? CreateViewController
+        }
+        
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+    }
+ 
 }
