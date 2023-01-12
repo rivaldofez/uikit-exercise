3f0fdbb92a077e06cdbc2811e9b2f61b6c81f453
rivaldofez <rivaldofez@gmail.com>
2023-01-12 23:23:22 +0700

    create project swiftui comparison

---
+++ SwiftUIComparison/SwiftUIComparison/SwiftUIComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComparisonApp.swift
+//  SwiftUIComparison
+//
+//  Created by Rivaldo Fernandes on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
