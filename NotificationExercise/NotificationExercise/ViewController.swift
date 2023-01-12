dc153d20fa0694c45ebeb61ee0cd55bad8e19dc0
rivaldofez <rivaldofez@gmail.com>
2023-01-12 20:30:10 +0700

    implement notifications

--- NotificationExercise/NotificationExercise/ViewController.swift
+++ NotificationExercise/NotificationExercise/ViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        UNUserNotificationCenter.current().delegate = self
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         UNUserNotificationCenter.current().requestAuthorization(options : [.alert, .badge, .sound]){ (granted, _) in
@@ -24,6 +28,46 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func scheduleNotification(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "Submission Anda Telah Diterima!"
+        content.body = "Selamat Anda telah menyelesaikan kelas Belajar Fundamental Aplikasi iOS."
+        content.sound = .default
+        content.userInfo = ["value": "Data dengan local notification"]
+        
+        let fireDate = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second], from: Date().addingTimeInterval(10))
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: fireDate, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "message", content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { error in
+            if error != nil {
+                print("Error = \(error?.localizedDescription ?? "Terjadi kesalahan dalam local notification")")
+            }
+        }
+        
+        print("button pressed")
+    }
+}
+
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .badge, .sound])
+    }
     
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        print("UserInfo yang terkait dengan notifikasi == \(response.notification.request.content.userInfo)")
+        completionHandler()
+    }
 }
 
