347d4e9a4ab6fe177b64dda98243450738e3dbdf
rivaldofez <rivaldofez@gmail.com>
2023-01-23 22:43:22 +0700

    test project using to add cocoapods

--- GreetingAppInstrumentTest/GreetingAppInstrumentTest/ViewController.swift
+++ GreetingAppInstrumentTest/GreetingAppInstrumentTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        haloButton.isEnabled = false
+//        haloButton.isEnabled = false
     }
 
     @IBAction func showHalo(_ sender: Any) {
