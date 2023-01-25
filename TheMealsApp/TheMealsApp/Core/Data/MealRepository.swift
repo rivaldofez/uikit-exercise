8b108008c6ea299db60c8dae5cc52616631ee403
rivaldofez <rivaldofez@gmail.com>
2023-01-26 00:07:17 +0700

    change meal repository with observable rxswift

--- TheMealsApp/TheMealsApp/Core/Data/MealRepository.swift
+++ TheMealsApp/TheMealsApp/Core/Data/MealRepository.swift
@@ -5,12 +5,10 @@
 //  Created by Gilang Ramadhan on 22/11/22.
 //
 
-import Foundation
+import RxSwift
 
 protocol MealRepositoryProtocol {
-    
-    func getCategories(result: @escaping (Result<[CategoryModel], Error>) -> Void)
-    
+    func getCategories() -> Observable<[CategoryModel]>
 }
 //
 //final class MealRepository: NSObject {
@@ -48,61 +46,34 @@ final class MealRepository: NSObject {
 
 extension MealRepository: MealRepositoryProtocol {
     
-//    func getCategories(
-//        result: @escaping (Result<[CategoryModel], Error>) -> Void
-//    ) {
-//
-//        self.remote.getCategories { remoteResponses in
-//            switch remoteResponses {
-//            case .success(let categoryResponses):
-//                let resultList = CategoryMapper.mapCategoryResponsesToDomains(input: categoryResponses)
-//                result(.success(resultList))
-//            case .failure(let error):
-//                result(.failure(error))
-//            }
-//        }
-//    }
+    //    func getCategories(
+    //        result: @escaping (Result<[CategoryModel], Error>) -> Void
+    //    ) {
+    //
+    //        self.remote.getCategories { remoteResponses in
+    //            switch remoteResponses {
+    //            case .success(let categoryResponses):
+    //                let resultList = CategoryMapper.mapCategoryResponsesToDomains(input: categoryResponses)
+    //                result(.success(resultList))
+    //            case .failure(let error):
+    //                result(.failure(error))
+    //            }
+    //        }
+    //    }
     
-    func getCategories(
-      result: @escaping (Result<[CategoryModel], Error>) -> Void
-    ) {
-      locale.getCategories { localeResponses in
-        switch localeResponses {
-        case .success(let categoryEntity):
-          let categoryList = CategoryMapper.mapCategoryEntitiesToDomains(input: categoryEntity)
-          if categoryList.isEmpty {
-            self.remote.getCategories { remoteResponses in
-              switch remoteResponses {
-              case .success(let categoryResponses):
-                let categoryEntities = CategoryMapper.mapCategoryResponsesToEntities(input: categoryResponses)
-                self.locale.addCategories(from: categoryEntities) { addState in
-                  switch addState {
-                  case .success(let resultFromAdd):
-                    if resultFromAdd {
-                      self.locale.getCategories { localeResponses in
-                        switch localeResponses {
-                        case .success(let categoryEntity):
-                          let resultList = CategoryMapper.mapCategoryEntitiesToDomains(input: categoryEntity)
-                          result(.success(resultList))
-                        case .failure(let error):
-                          result(.failure(error))
-                        }
-                      }
-                    }
-                  case .failure(let error):
-                    result(.failure(error))
-                  }
+    func getCategories() -> Observable<[CategoryModel]> {
+        return self.locale.getCategories()
+            .map { CategoryMapper.mapCategoryEntitiesToDomains(input: $0) }
+            .filter { !$0.isEmpty }
+            .ifEmpty(switchTo: self.remote.getCategories()
+                .map { CategoryMapper.mapCategoryResponsesToEntities(input: $0) }
+                .flatMap { self.locale.addCategories(from: $0) }
+                .filter { $0 }
+                .flatMap { _ in self.locale.getCategories()
+                        .map { CategoryMapper.mapCategoryEntitiesToDomains(input: $0) }
                 }
-              case .failure(let error):
-                result(.failure(error))
-              }
-            }
-          } else {
-            result(.success(categoryList))
-          }
-        case .failure(let error):
-          result(.failure(error))
-        }
-      }
+            )
     }
+    
+    
 }
